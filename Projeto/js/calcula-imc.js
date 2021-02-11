7d1e8b962c18c533765e7f829adc03af713371c8
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-11 19:05:32 -0300

    concluído aula 05

--- Projeto/js/calcula-imc.js
+++ Projeto/js/calcula-imc.js
@@ -1,10 +1,15 @@
-var peso = 100;
-var altura = 1.90;
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
 
 var ehDifDeZero = (altura !=0); // se a altura for 0, é atribuido false à variável
 
 if(ehDifDeZero){
     var imc = peso / (altura * altura);
+    var tdImc = document.getElementById("imc-2");
+    tdImc.textContent = imc;
     console.log(imc);
 } else{
     console.log("Não executei porque a altura eh igual a zero");
