32be11fd8af4414e41a858e8267b91a5908df2c2
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-12 19:41:43 -0300

    concluído aula 17 - Adicionando Pacientes e Utilizando querySelector

--- Projeto/js/adiciona.js
+++ Projeto/js/adiciona.js
@@ -5,11 +5,16 @@ botao.addEventListener("click", function(){
 
     window.event.preventDefault(); // impede que o botão faça o seu comportamento padrão de dar "submit", que no caso atualizaria a página e perderíamos a inclusão do paciente
 
+    var campoNome = document.querySelector("#nome");
+    var campoPeso = document.querySelector("#peso");
+    var campoAltura = document.querySelector("#altura");
+    var campoGordura = document.querySelector("#gordura");
+
     var pacienteNovo =  "<tr class='paciente' >"+
-                        "<td class='info-nome'>João</td>"+
-                        "<td class='info-peso'>80</td>"+
-                        "<td class='info-altura'>1.72</td>"+
-                        "<td class='info-gordura'>40</td>"+
+                        "<td class='info-nome'>"+ campoNome.value +"</td>"+
+                        "<td class='info-peso'>"+ campoPeso.value +"</td>"+
+                        "<td class='info-altura'>"+ campoAltura.value +"</td>"+
+                        "<td class='info-gordura'>"+ campoGordura.value +"</td>"+
                         "<td class='info-imc'>0</td>"+
                         "</tr>";
     
@@ -17,4 +22,9 @@ botao.addEventListener("click", function(){
     var tabela = document.querySelector("tbody"); //o querySelector pega o primeiro elemento que ele acha, já o getElementsByTagName retorna um array. Mas se eu usar querySelectorAll ele retornará um array
     //tabela.textContent = tabela.textContent + pacienteNovo; //pega a lista de pacientes atuais e adiciona o ultimo inserido //não vai funcionar pq o textContent é só pra texto, e estamos usando uma tabela
     tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+
+    campoNome.value = "";
+    campoPeso.value = "";
+    campoAltura.value = "";
+    campoGordura.value = "";
 })
