c35ef76cc92e1f2a86c72f349f73ecf5129fadc8
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-12 04:05:07 -0300

    concluído aulas 09 e 10

--- Projeto/js/calcula-imc.js
+++ Projeto/js/calcula-imc.js
@@ -1,26 +1,17 @@
-/*
-var tdPeso = document.getElementById("peso-1");
-var tdAltura = document.getElementById("altura-1");
-
-var peso1 = tdPeso.textContent;
-var altura1 = tdAltura.textContent;
-var paciente1 = {peso:peso1, altura:altura1};
-
-
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
-
-var peso2 = tdPeso.textContent;
-var altura2 = tdAltura.textContent;
-var paciente2 = {peso:peso2, altura:altura2};
-
-var pacientes = [paciente1, paciente2];
-*/
+function calculaIMC(paciente){ //a funcao recebe um argumento chamado paciente
+    var ehDifDeZero = (altura !=0); // se a altura for 0, é atribuido false à variável
+    if(ehDifDeZero){
+        var imc = paciente.peso / (paciente.altura * paciente.altura); 
+        return imc;     
+        
+    } else{
+        console.log("Não executei porque a altura eh igual a zero");
+    }
+}
 
 var trsPacientes = document.getElementsByClassName("paciente"); // Array de trs da classe paciente
-
-var posAtual = 0;
-while(posAtual <= trsPacientes.length - 1){ // o array é de tamanho 2, mas a última posição é a posição 1. Enquanto a posAtual for menor ou igual a 1, ele executa o código, pegando os 2 objetos do array
+ 
+for(var posAtual = 0; posAtual <= trsPacientes.length -1; posAtual++) { // o array é de tamanho 2, mas a última posição é a posição 1. Enquanto a posAtual for menor ou igual a 1, ele executa o código, pegando os 2 objetos do array
     var pacienteTr = trsPacientes[posAtual]; // vai pegando cada um dos pacientes contidos em cada TR
 
     var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; // dentro de cada paciente, pega a informação contida na classe info-name
@@ -29,16 +20,10 @@ while(posAtual <= trsPacientes.length - 1){ // o array é de tamanho 2, mas a ú
     
     var paciente = {nome:tdNome.textContent, peso:tdPeso.textContent, altura:tdAltura.textContent}; // criei o objeto paciente que contem as características que puxei via getElementsByClassName
     
-    var ehDifDeZero = (altura !=0); // se a altura for 0, é atribuido false à variável
-    if(ehDifDeZero){
-        var imc = paciente.peso / (paciente.altura * paciente.altura);      
-        
-        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-        tdImc.textContent = imc;
-        console.log(imc);
-    } else{
-        console.log("Não executei porque a altura eh igual a zero");
-    }
-    posAtual++;
+    var imc = calculaIMC(paciente); //chama a função passando cada um dos pacientes como parametro. Como eu preciso do valor do imc aqui fora, eu faço essa chamada dentro de uma var, pra receber o return 
+
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+    tdImc.textContent = imc;
+    console.log(imc);
 }
 
